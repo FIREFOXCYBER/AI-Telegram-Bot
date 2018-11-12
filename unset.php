e6ff8c18f0c2d9b40f18d2b3923000525350f4fb
F[!R]3FoX <12250394+FIREFOXCYBER@users.noreply.github.com>
2018-11-12 18:11:05 +0330

    Initial commit

---
+++ unset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * README
+ * This file is intended to unset the webhook.
+ * Uncommented parameters must be filled
+ */
+
+// Load composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Add you bot's API key and name
+$bot_api_key  = '631207254:AAED_UKx1YGBb7vfZoJsjztXmWPiVqYAndY';
+$bot_username = 'KowlaskiBot';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Delete webhook
+    $result = $telegram->deleteWebhook();
+
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e->getMessage();
+}
